ebadf93e74869331e15075fe2189c5ecea6ea7e4
rodrigues217 <joaoprodrigues237@gmail.com>
2025-06-22 19:05:04 -0300

    Update CompradorController.java

--- src/main/java/org/example/Controller/CompradorController.java
+++ src/main/java/org/example/Controller/CompradorController.java
@@ -13,24 +13,31 @@ public class CompradorController {
         this.compradorService = new CompradorService();
     }
 
-    public void criarComprador(String nome, String telefone, String cpf) throws Exception {
-        compradorService.criarComprador(nome, telefone, cpf);
+    public CompradorMODEL criarComprador(String nome, String telefone, String cpf, String email, String endereco) throws Exception {
+        return compradorService.criarComprador(nome, telefone, cpf, email, endereco);
     }
 
-    public List<CompradorMODEL> listarCompradores() {
-        return compradorService.listarCompradores();
+    public List<CompradorMODEL> listarTodosCompradores() {
+        return compradorService.listarTodosCompradores();
     }
 
-    public void atualizarComprador(Long id, String novoNome, String novoTelefone) throws Exception {
-        compradorService.atualizarComprador(id, novoNome, novoTelefone);
+    public CompradorMODEL atualizarComprador(Long id, String novoNome, String novoTelefone, String novoCpf, String novoEmail, String novoEndereco) throws Exception {
+        return compradorService.atualizarComprador(id, novoNome, novoTelefone, novoCpf, novoEmail, novoEndereco);
     }
 
-
-    public void deletarComprador(Long id) throws Exception {
-        compradorService.deletarComprador(id);
+    public void removerComprador(Long id) throws Exception {
+        compradorService.removerComprador(id);
     }
 
-    public CompradorMODEL buscarCompradorPorCPF(String cpf) throws Exception {
+    public CompradorMODEL buscarCompradorPorId(Long id) {
+        return compradorService.buscarCompradorPorId(id);
+    }
+
+    public CompradorMODEL buscarCompradorPorCPF(String cpf) {
         return compradorService.buscarCompradorPorCPF(cpf);
+    }
+
+    public boolean verificarCpfExistente(String cpf, Long idAtual) {
+        return compradorService.verificarCpfExistente(cpf, idAtual);
     }
 }
